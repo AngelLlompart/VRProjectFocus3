3c0524f44ca213f6f8914b542eee10b4397b26b7
Angel <angelllompart@paucasesnovescifp.cat>
2022-11-21 12:26:17 +0100

    Add sound effects

---
+++ Assets/Scripts/CanHit.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanHit : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Dart"))
+        {
+            _audioSource.Play();
+        }
+    }
+}
